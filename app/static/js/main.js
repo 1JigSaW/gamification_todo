5ed737746e0fd07051e8e6288dd110661c92e332
1JigSaW <misho_1998@mail.ru>
2022-07-20 23:27:04 +0300

    add js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,24 @@
 function complete_task(clicked_id) {
 	if (document.getElementById(clicked_id).style.textDecoration === 'none') {
-		
 		document.getElementById(clicked_id).style.textDecoration = "line-through";
 	} else {
 		document.getElementById(clicked_id).style.textDecoration = 'none';
 	}
+	let form = document.getElementById(clicked_id); // selecting the form
+
+	form.addEventListener('submit', function(event) { // 1
+	    event.preventDefault()
+	    
+	    let data = new FormData(); // 2
+	    
+	    data.append("title", document.getElementById('title').value)  
+	    data.append("note", document.getElementById('note').value)
+	    data.append("csrfmiddlewaretoken", '{{csrf_token}}') // 3
+	    
+	    axios.post('create_note/', data) // 4
+	     .then(res => alert("Form Submitted")) // 5
+	     .catch(errors => console.log(errors)) // 6
+
+	})
+
 }
--- app/views.py
+++ app/views.py
@@ -4,5 +4,7 @@ from app.models import Purpose, Metrics
 def index(request):
 	purposes = Purpose.objects.all()
 	metrics = Metrics.objects.all()
+	if request.method == 'POST':
+		print('aaa')
 	return render(request, 'index.html', {'purposes': purposes, 
 		'metrics': metrics})
